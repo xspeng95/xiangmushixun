f1907fc60958c5b81d398cc980534f1887004d75
legendpeng <873806889@qq.com>
2018-06-29 10:23:31 +0800

    the view of the sign in

---
+++ ChatDemo/app/src/main/java/com/example/legendpeng/chatdemo/RegisterView.java
@@ -0,0 +1,32 @@
+package com.example.legendpeng.chatdemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class RegisterView extends AppCompatActivity {
+
+    private EditText username_text;
+    private EditText phone_text;
+    private EditText conform_text;
+    private EditText mima_text;
+    private EditText mimaconform_text;
+    private Button getinf_bu;
+    private  Button rigister_bu;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register_view);
+
+        username_text=(EditText)findViewById(R.id.name_text);
+        phone_text=(EditText)findViewById(R.id.phone_text);
+        conform_text=(EditText)findViewById(R.id.conform_text);
+        mima_text=(EditText)findViewById(R.id.mima_text);
+        mimaconform_text=(EditText)findViewById(R.id.mimaconform_text);
+        getinf_bu=(Button)findViewById(R.id.getinf_bu);
+        rigister_bu=(Button)findViewById(R.id.rigister_bu);
+
+    }
+}
